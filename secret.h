67026ea3dbb32d2814d5caecfee623e3639105e6
Akshit051604 <akshitbaren0516@gmail.com>
2024-08-28 00:58:08 +0530

    Update secret.h

--- secret.h
+++ secret.h
@@ -1,5 +1,5 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "Akshit_5G"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "Divyanshi"	// replace MyPassword with your WiFi password
+#define SECRET_SSID "Akshit_5G"		
+#define SECRET_PASS "Divyanshi"	
