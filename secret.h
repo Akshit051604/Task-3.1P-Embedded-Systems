8c3743aef91c499188f3572021b7db44e848640a
Akshit051604 <akshitbaren0516@gmail.com>
2024-08-28 01:19:02 +0530

    Update secret.h

--- secret.h
+++ secret.h
@@ -1,5 +1,4 @@
-// Use this file to store all of the private credentials 
-// and connection details
+// Use this file to store all of the private credentials and connection details
 
 #define SECRET_SSID "Akshit_5G"		
 #define SECRET_PASS "Divyanshi"	
