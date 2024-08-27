2c63cada896a7a9db5059696c91bab58638d9aee
Akshit051604 <akshitbaren0516@gmail.com>
2024-08-28 01:18:22 +0530

    Update and rename wire.ino to Work.ino

--- Work.ino
+++ Work.ino
@@ -6,14 +6,13 @@ char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
 WiFiClient client;
-//https://maker.ifttt.com/trigger/TASK_3.1P/with/key/dCzgH4VqCd8yZ_jYfZseMYt86OHL13Bdzn5tjtvUlSZ
 
-char   HOST_NAME[] = "maker.ifttt.com";
+char HOST_NAME[] = "maker.ifttt.com";
 
 // Data variables for IFTTT
 String light_state = "Off";
-int light_value = 0;
-String last_light = "Off";
+int value_of_light = 0;
+String last_stateof_light = "Off";
 
 bool connectToWiFi(char ssid[], char pass[]) {
   if (WiFi.status() != WL_CONNECTED) {
@@ -36,11 +35,11 @@ bool connectToWiFi(char ssid[], char pass[]) {
   }
   return true; 
 }
-int readLightSensor() {
-  light_value = digitalRead(LIGHT_SENSOR);
-  return light_value;
-}
 
+int SenseLight() {
+  value_of_light = digitalRead(LIGHT_SENSOR);
+  return value_of_light;
+}
 
 void sendWebhook(String state) {
   if (!client.connect(HOST_NAME, 80)) {
@@ -48,33 +47,26 @@ void sendWebhook(String state) {
     return;
   }
 
+  Serial.println("Connected to server");
+  client.println("GET /trigger/Light_Changed/with/key/dCzgH4VqCd8yZ_jYfZseMYt86OHL13Bdzn5tjtvUlSZ HTTP/1.1");
+  client.println("Host: maker.ifttt.com");
+  client.println("Connection: close");
+  client.println();
 
-    Serial.println("Connected to server");
-    client.println("GET /trigger/Light_Changed/with/key/dCzgH4VqCd8yZ_jYfZseMYt86OHL13Bdzn5tjtvUlSZ HTTP/1.1");
-    client.println("Host: maker.ifttt.com"  );
-    client.println("Connection: close");
-    client.println();
-
-  
   client.stop();
   Serial.println("Sent notification to IFTTT");
 }
 
 void setup() {
-
   Serial.begin(115200);
-  connectToWiFi(ssid,pass);
+  connectToWiFi(ssid, pass);
   while (!Serial);
 
-
-  pinMode(LIGHT_SENSOR,INPUT);
+  pinMode(LIGHT_SENSOR, INPUT);
 }
 
-
-
 void loop() {
-
-  int current_light = readLightSensor();
+  int current_light = SenseLight();
 
   if (current_light > 0) {
     light_state = "On";
@@ -82,11 +74,5 @@ void loop() {
     light_state = "Off";
   }
 
- 
-  if (light_state != last_light || Serial.read() == 'c') {
-    sendWebhook(light_state);
-    last_light = light_state;
-  }
-  delay(1000); 
-
-}
+  if (light_state != last_stateof_light || Serial.read() == 'c') {
+    sendWebhook(lig
